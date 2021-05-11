befd7bf244d8856a70387b29f7e801097e5b7f15
AirellAristo <78353843+AirellAristo@users.noreply.github.com>
2021-05-11 19:23:09 +0700

    Add files via upload

---
+++ LAB 12-Set.py
@@ -0,0 +1,19 @@
+#Nama  : Airell Aristo Subagia 
+#NIM   : 71200609 
+#Topik : Set 
+
+#Soal 
+#Pada Suatu Hari Adikmu diberi tugas oleh gurunya untuk menyelesaikan suatu himpunan.
+#Soal yang diberikan adalah a = {1,2,3,4} dan b = {3,4,5,6}
+#Bantulah adikmu mengerjakan prnya
+
+def himpunan(a,b) : 
+    print(a & b)
+    print('a = ',a - b)
+    print('b = ',b - a)
+
+
+
+a = {1,2,3,4}
+b = {3,4,5,6}
+himpunan(a,b)
